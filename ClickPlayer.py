779d98888e17298884bee43e887811f8adaf205d
andrew.livesay <andrewlivesay3@gmail.com>
2026-03-01 12:15:25 -0600

    Finished first version of the auto clicker and click player. Going to refactor into clean classes, add loop, increase speed, and add keybinds to make sure it can be controlled.

--- ClickPlayer.py
+++ ClickPlayer.py
@@ -2,17 +2,21 @@ import time
 import json
 import pyautogui
 
+pyautogui.FAILSAFE = True
+
 with open("mouse_recording.json", "r") as f:
     events = json.load(f)
     
 print("Starting replay in 3 seconds...")
 time.sleep(3)
 
-start_time = time.time()
+previous_time = 0
 
 for event in events:
-    while time.time() - start_time < event["time"]:
-        pass
+    #Calculate delay between this event and previous one.
+    delay = event["time"] - previous_time
+    time.sleep(delay)
+    previous_time = event["time"]
     
     if event["type"] == "move":
         pyautogui.moveTo(event["x"], event["y"])
