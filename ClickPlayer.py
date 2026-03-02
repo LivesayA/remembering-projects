e1189970fa2351ca849c8d7e405e39dd8dbdc566
andrew.livesay <andrewlivesay3@gmail.com>
2026-03-02 10:37:10 -0600

    Commenting out uneccessary lines of code.

--- ClickPlayer.py
+++ ClickPlayer.py
@@ -50,26 +50,26 @@ if __name__ == "__main__":
     
     player.play()
 
-with open("mouse_recording.json", "r") as f:
-    events = json.load(f)
+# with open("mouse_recording.json", "r") as f:
+#     events = json.load(f)
     
-print("Starting replay in 3 seconds...")
-time.sleep(3)
+# print("Starting replay in 3 seconds...")
+# time.sleep(3)
 
-#previous_time = 0
+# previous_time = 0
 
-#for event in events:
-    #Calculate delay between this event and previous one.
-#    delay = event["time"] - previous_time
-#    time.sleep(delay)
-#    previous_time = event["time"]
-#    
-#    if event["type"] == "move":
-#        pyautogui.moveTo(event["x"], event["y"])
-#    elif event["type"] == "click":
-#        if event["pressed"]:
-#            pyautogui.mouseDown(event["x"], event["y"])
-#        else:
-#            pyautogui.mouseUp(event["x"], event["y"])
+# for event in events:
+#     #Calculate delay between this event and previous one.
+#     delay = event["time"] - previous_time
+#     time.sleep(delay)
+#     previous_time = event["time"]
+    
+#     if event["type"] == "move":
+#         pyautogui.moveTo(event["x"], event["y"])
+#     elif event["type"] == "click":
+#         if event["pressed"]:
+#             pyautogui.mouseDown(event["x"], event["y"])
+#         else:
+#             pyautogui.mouseUp(event["x"], event["y"])
 
-#print("Replay finished.")
+# print("Replay finished.")
